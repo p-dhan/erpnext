2357ed8f762c09dcc46cca6cac3cb420b173b45d
p-dhan <103286990+p-dhan@users.noreply.github.com>
2026-01-15 19:46:44 +0000

    commit (#4)
    * print gantt chart
    
    * download
    
    * update print
    
    * download as pdf
    
    * add dhtmlxgantt
    
    * update hooks
    
    * update hooks
    
    * use https
    
    * add order by for payment ledger
    
    * make sales invoice first
    
    * remove check on previous operation
    
    * use in progress job card
    
    * fix query
    
    * check completed qty from job card
    
    * fix query
    
    * fix column name
    
    * remove svg gantt print js
    
    * fix lint
    
    * fix lint
    
    * fix lint
    
    * fix lint
    
    * remove svg export
    
    * remove unused var
    
    * remove date condition for depreciation entry
    
    * add 1 year to create dep
    
    * remove fix
    
    * remove depends on
    
    * remove setting default account
    
    * fix job work sourced item valuation
    
    * remove completed qty validation
    
    * revert the commit
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix divide by zero
    
    * add customer provided RM
    
    * fix
    
    * remove async processing
    
    * add desc to update table
    
    * revert
    
    * remove WO seq validation
    
    * fix amount calculation
    
    * remove redundant return statement in start_auto_reconcile function
    
    * fix lint
    
    * fix lint

--- erpnext/selling/doctype/sales_order/sales_order.py
+++ erpnext/selling/doctype/sales_order/sales_order.py
@@ -1677,7 +1677,7 @@ def update_status(status, name):
 
 
 @frappe.whitelist()
-def make_raw_material_request(items, company, sales_order, project=None):
+def make_raw_material_request(items, company, sales_order, project=None, material_request_type="Purchase"):
 	if not frappe.has_permission("Sales Order", "write"):
 		frappe.throw(_("Not permitted"), frappe.PermissionError)
 
@@ -1707,7 +1707,7 @@ def make_raw_material_request(items, company, sales_order, project=None):
 			doctype="Material Request",
 			transaction_date=nowdate(),
 			company=company,
-			material_request_type="Purchase",
+			material_request_type=material_request_type,
 		)
 	)
 	for item in raw_materials:
